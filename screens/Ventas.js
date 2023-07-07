71349d1a991595d9c6a4a6934da0d619b9e8d911
art028 <120035338@upq.edu.mx>
2023-07-06 19:29:25 -0600

    Exportar el pdf de ventas funcionando

--- screens/Ventas.js
+++ screens/Ventas.js
@@ -1,47 +1,52 @@
-import * as React from 'react';
+import React from 'react';
+import jsPDF from "jspdf";
+import "jspdf-autotable";
 import { DataTable, Text, Button } from 'react-native-paper';
-import RNHTMLtoPDF from 'react-native-html-to-pdf';
 
-const MyComponent = () => {
-  const [totalGeneral, setTotalGeneral] = React.useState(0);
-  
-  const exportToPDF = async () => {
-    const options = {
-      html: '<html><body><h1>Reporte de Ventas</h1><table><tr><th>Producto</th><th>Cantidad</th><th>Precio</th><th>Total</th></tr><tr><td>Refresco</td><td>2</td><td>15</td><td>30</td></tr><tr><td>Papas</td><td>4</td><td>20</td><td>80</td></tr></table><p>Total General: ' + totalGeneral + '</p></body></html>',
-      fileName: 'reporte_ventas',
-      directory: 'Documents',
+class Otro extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      people: [
+        { name: "Refresco", cant: "2",pre:"15",tot:"30" },
+        { name: "Torta", cant: "1",pre:"25",tot:"25" }
+      ]
     };
-    
-    const file = await RNHTMLtoPDF.convert(options);
-    console.log(file.filePath);
+  }
+
+  exportPDF = () => {
+    const unit = "pt";
+    const size = "A4"; // Use A1, A2, A3 or A4
+    const orientation = "portrait"; // portrait or landscape
+
+    const marginLeft = 40;
+    const doc = new jsPDF(orientation, unit, size);
+
+    doc.setFontSize(15);
+
+    const title = "Reporte de Ventas";
+    const headers = [["Producto", "Cantidad","Precio","Total"]];
+
+    const data = this.state.people.map((elt) => [elt.name, elt.cant,elt.pre,elt.tot]);
+
+    let content = {
+      startY: 50,
+      head: headers,
+      body: data
+    };
+
+    doc.text(title, marginLeft, 40);
+    doc.autoTable(content);
+    doc.save("report.pdf");
   };
 
-  return (
-    <>
-      <Text variant="headlineMedium">Reporte de Ventas</Text>
-      <DataTable>
-        <DataTable.Header>
-          <DataTable.Title>Producto</DataTable.Title>
-          <DataTable.Title numeric>Cantidad</DataTable.Title>
-          <DataTable.Title numeric>Precio</DataTable.Title>
-          <DataTable.Title numeric>Total</DataTable.Title>
-        </DataTable.Header>
-        <DataTable.Row>
-          <DataTable.Cell numeric>Refresco</DataTable.Cell>
-          <DataTable.Cell numeric>2</DataTable.Cell>
-          <DataTable.Cell numeric>15</DataTable.Cell>
-          <DataTable.Cell numeric>30</DataTable.Cell>
-        </DataTable.Row>
-        <DataTable.Row>
-          <DataTable.Cell numeric>Papas</DataTable.Cell>
-          <DataTable.Cell numeric>4</DataTable.Cell>
-          <DataTable.Cell numeric>20</DataTable.Cell>
-          <DataTable.Cell numeric>80</DataTable.Cell>
-        </DataTable.Row>
-        </DataTable>
-      <Button onPress={exportToPDF}>Exportar a PDF</Button>
-    </>
-  );
-};
+  render() {
+    return (
+      <div>
+        <button onClick={() => this.exportPDF()}>Generate Report</button>
+      </div>
+    );
+  }
+}
 
-export default MyComponent;
+export default Otro;
